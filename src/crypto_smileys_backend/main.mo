9f071b211b0306054d65c05d48bbca3700326c04
Stefan Penzinger <Stefan.Penzinger@fh-hagenberg.at>
2024-05-23 15:58:24 +0200

    Update files to represent final state

--- src/crypto_smileys_backend/main.mo
+++ src/crypto_smileys_backend/main.mo
@@ -1,5 +1,14 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Random "mo:base/Random";
+import Text "mo:base/Text";
+
+actor SmileyGenerator {
+  private func generate_hash(str : Text) : Nat {
+    let seed = Text.encodeUtf8(str);
+	return Random.rangeFrom(27, seed);
+  };
+
+  public func get_smiley_hash(name : Text) : async Nat {
+    let hash = generate_hash(name);
+	return hash;
   };
 };
--- src/crypto_smileys_frontend/src/App.js
+++ src/crypto_smileys_frontend/src/App.js
@@ -7,12 +7,66 @@ class App {
 
   constructor() {
     this.#render();
+    this.parts = {
+        head: [
+            ["⧙", "⧘"],
+            ["༼", "༽"],
+            ["ᖗ", "ᖘ"],
+            ["ᕳ", "ᕲ"],
+            ["(", ")"],
+        ],
+        eyes: [
+            ["•", "•"],
+            ["ಠ", "ಠ"],
+            [" ͡°", " ͡°"],
+            ["o", "Ô"],
+            ["ಥ", "ಥ"],
+        ],
+        nose: [
+            "◡",
+            "‸",
+            "ᴥ",
+            "⚇",
+            "👄"
+        ],
+        hands: [
+            ["乁", "ㄏ"],
+            ["ψ", "ψ"],
+            ["〈", "ノ･:*☆"],
+            ["ᕙ", "ᕗ"],
+            ["┌∩┐", "┌∩┐"]
+        ]
+    };
+
+  }
+
+  generateSmiley(number) {
+    const numString = number.toString()
+    const headIndex = parseInt(numString.substring(0, 2))
+    const eyesIndex = parseInt(numString.substring(2, 4))
+    const noseIndex = parseInt(numString.substring(4, 6))
+    const handsIndex = parseInt(numString.substring(6, 8))
+    const handsLength = this.parts.hands.length
+    const headLength = this.parts.head.length
+    const eyesLength = this.parts.eyes.length
+    const noseLength = this.parts.nose.length
+
+    
+    const smiley = this.parts.hands[handsIndex % handsLength][0] +
+		  this.parts.head[headIndex % headLength][0] +
+		  this.parts.eyes[eyesIndex % eyesLength][0] +
+		  this.parts.nose[noseIndex % noseLength] +
+		  this.parts.eyes[eyesIndex % eyesLength][1] +
+		  this.parts.head[headIndex % headLength][1] +
+		  this.parts.hands[handsIndex % handsLength][1]
+
+    return smiley;
   }
 
   #handleSubmit = async (e) => {
     e.preventDefault();
     const name = document.getElementById('name').value;
-    this.greeting = await crypto_smileys_backend.greet(name);
+    this.smiley = this.generateSmiley(await crypto_smileys_backend.get_smiley_hash(name));
     this.#render();
   };
 
@@ -27,7 +81,7 @@ class App {
           <input id="name" alt="Name" type="text" />
           <button type="submit">Click Me!</button>
         </form>
-        <section id="greeting">${this.greeting}</section>
+        <section id="greeting">${this.smiley}</section>
       </main>
     `;
     render(body, document.getElementById('root'));
